d084160a8921f6036eb5610ad0d713e6f487a09a
tsiguenz <tsiguenz@student.42.fr>
2024-02-18 18:21:08 +0100

    delete forbidden functions

--- module00/ex00/myawesomescript.sh
+++ module00/ex00/myawesomescript.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-URL=$(curl --silent --location --head "$1" --output /dev/null -w "%{url_effective}\n")
-
-if [ $? -ne 0 ]; then
-	echo "Error bad url!"
-	exit 1
-fi
-
-echo "$URL"
+curl --silent --location --head "$1" --output /dev/null -w "%{url_effective}\n"
