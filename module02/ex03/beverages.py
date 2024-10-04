58582d233da0b3fe8a988086a904f99f96595f2b
tsiguenz <tsiguenz@student.42.fr>
2024-10-04 15:15:58 +0200

    mod02/ex03 fix serve function take only HotBeverage

--- module02/ex03/beverages.py
+++ module02/ex03/beverages.py
@@ -1,40 +1,40 @@
 class HotBeverage:
-    name = 'hot beverage'
-    price = '0.30'
+    name = "hot beverage"
+    price = "0.30"
 
     def Description(self) -> str:
-        return 'Just some hot water in a cup.'
+        return "Just some hot water in a cup."
 
     def __str__(self):
-        str = f'name : {self.name}\n'
-        str += f'price : {self.price}\n'
-        str += f'description : {self.Description()}'
+        str = f"name : {self.name}\n"
+        str += f"price : {self.price}\n"
+        str += f"description : {self.Description()}"
         return str
 
 
 class Coffee(HotBeverage):
-    name = 'coffee'
-    price = '0.40'
+    name = "coffee"
+    price = "0.40"
 
     def Description(self) -> str:
-        return 'A coffee, to stay awake.'
+        return "A coffee, to stay awake."
 
 
 class Tea(HotBeverage):
-    name = 'tea'
+    name = "tea"
 
 
 class Chocolate(HotBeverage):
-    name = 'chocolate'
-    price = '0.50'
+    name = "chocolate"
+    price = "0.50"
 
     def Description(self) -> str:
-        return 'Chocolate, sweet chocolate...'
+        return "Chocolate, sweet chocolate..."
 
 
 class Cappuccino(HotBeverage):
-    name = 'cappuccino'
-    price = '0.45'
+    name = "cappuccino"
+    price = "0.45"
 
     def Description(self) -> str:
         return "Un po' di Italia nella sua tazza!"
--- module02/ex03/machine.py
+++ module02/ex03/machine.py
@@ -21,18 +21,20 @@ class CoffeeMachine:
     def repair(self):
         self.nb_services = 0
 
-    def serve(self, beverage: beverages.HotBeverage):
+    def serve(self, beverage):
+        if not is_derived_of(beverage, beverages.HotBeverage):
+            raise TypeError(f"{type(beverage)} is not a derived of HotBeverage")
         if self.nb_services >= 10:
             raise self.BrokenMachineException
         self.nb_services += 1
-        return random.choice([self.EmptyCup(), beverage])
+        return random.choice([self.EmptyCup, beverage])()
 
 
-def is_child_of(to_check, base_type):
+def is_derived_of(to_check, base_class):
     return (
         isinstance(to_check, type)
-        and issubclass(to_check, base_type)
-        and to_check is not base_type
+        and issubclass(to_check, base_class)
+        and to_check is not base_class
     )
 
 
@@ -40,9 +42,9 @@ def get_list_of_available_beverages(lst_items):
     available_beverages = []
     for item in lst_items:
         drink_name = item[0]
-        if not is_child_of(item[1], beverages.HotBeverage):
+        if not is_derived_of(item[1], beverages.HotBeverage):
             continue
-        available_beverages.append(beverages.__dict__[drink_name]())
+        available_beverages.append(beverages.__dict__[drink_name])
     return available_beverages
 
 
@@ -54,7 +56,9 @@ if __name__ == "__main__":
         try:
             beverage = machine.serve(random.choice(available_beverages))
             print(f"i = {i}, {beverage}", end="\n\n")
-
+        except TypeError as e:
+            print(e)
+            exit(1)
         except Exception as e:
             print(e, end="\n\n")
             machine.repair()
