2ef6c6d3c90b135c87a6714716aa4234d3f77b77
tsiguenz <tsiguenz@student.42.fr>
2024-10-08 13:59:32 +0200

    mod02/ex02 format file

--- module02/ex02/beverages.py
+++ module02/ex02/beverages.py
@@ -1,50 +1,51 @@
 #!/usr/bin/python3
 
-class HotBeverage:
-    name = 'hot beverage'
-    price = '0.30'
 
-    def Description(self) -> str:
-        return 'Just some hot water in a cup.'
+class HotBeverage:
+    name = "hot beverage"
+    price = "0.30"
+
+    def description(self) -> str:
+        return "Just some hot water in a cup."
 
     def __str__(self):
-        str = f'name : {self.name}\n'
-        str += f'price : {self.price}\n'
-        str += f'description : {self.Description()}'
+        str = f"name : {self.name}\n"
+        str += f"price : {self.price}\n"
+        str += f"description : {self.description()}"
         return str
 
 
 class Coffee(HotBeverage):
-    name = 'coffee'
-    price = '0.40'
+    name = "coffee"
+    price = "0.40"
 
-    def Description(self) -> str:
-        return 'A coffee, to stay awake.'
+    def description(self) -> str:
+        return "A coffee, to stay awake."
 
 
 class Tea(HotBeverage):
-    name = 'tea'
+    name = "tea"
 
 
 class Chocolate(HotBeverage):
-    name = 'chocolate'
-    price = '0.50'
+    name = "chocolate"
+    price = "0.50"
 
-    def Description(self) -> str:
-        return 'Chocolate, sweet chocolate...'
+    def description(self) -> str:
+        return "Chocolate, sweet chocolate..."
 
 
 class Cappuccino(HotBeverage):
-    name = 'cappuccino'
-    price = '0.45'
+    name = "cappuccino"
+    price = "0.45"
 
-    def Description(self) -> str:
+    def description(self) -> str:
         return "Un po' di Italia nella sua tazza!"
 
 
-if __name__ == '__main__':
-    print(HotBeverage(), end='\n\n')
-    print(Coffee(), end='\n\n')
-    print(Tea(), end='\n\n')
-    print(Chocolate(), end='\n\n')
+if __name__ == "__main__":
+    print(HotBeverage(), end="\n\n")
+    print(Coffee(), end="\n\n")
+    print(Tea(), end="\n\n")
+    print(Chocolate(), end="\n\n")
     print(Cappuccino())
