09e0f71a86b329d949969840036fb900363c3cb9
tsiguenz <tsiguenz@student.42.fr>
2024-10-11 18:39:52 +0200

    mod03/ex02 format and refactoring

--- module03/ex02/request_wikipedia.py
+++ module03/ex02/request_wikipedia.py
@@ -10,15 +10,17 @@ def usage():
 
 
 def write_to_file(filename, content):
-    print(f'Writing content in {filename}')
+    print(f"Writing content in {filename}")
     try:
         with open(filename, "w") as f:
             f.write(content)
     except Exception as e:
         print("Can't write to file:", e)
+        exit(1)
+
 
 def get_wiki_content(term):
-    print('Get wikipedia page from API')
+    print("Get wikipedia page from API")
     URL = "https://en.wikipedia.org/w/api.php"
     PARAMS = {
         "action": "parse",
@@ -29,11 +31,11 @@ def get_wiki_content(term):
     }
     r = requests.get(url=URL, params=PARAMS)
     if r.status_code != 200:
-        print('Bad status code')
+        print("Bad status code")
         exit(1)
     json = r.json()
-    if 'error' in json.keys():
-        print('Error:', json['error']['info'])
+    if "error" in json.keys():
+        print("Error:", json["error"]["info"])
         exit(1)
     return json["parse"]["wikitext"]["*"]
 
@@ -43,7 +45,10 @@ if __name__ == "__main__":
         print("Invalid number of arguments")
         usage()
         exit(1)
-    term = sys.argv[1].strip().lower()
-    filename = term.replace(" ", "_") + ".wiki"
-    wikitext = get_wiki_content(term)
-    write_to_file(filename, dewiki.from_string(wikitext))
+    try:
+        term = sys.argv[1].strip().lower()
+        filename = "_".join(term.split()) + ".wiki"
+        wikitext = get_wiki_content(term)
+        write_to_file(filename, dewiki.from_string(wikitext))
+    except Exception as e:
+        print(e)
