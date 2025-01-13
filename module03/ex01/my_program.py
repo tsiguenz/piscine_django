95c33de10ec801fdd7ce35a6501d940676cf1946
tsiguenz <tsiguenz@student.42.fr>
2025-01-13 22:54:30 +0100

    d03 ex01 improve

--- module03/ex01/my_program.py
+++ module03/ex01/my_program.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-from local_lib.pathlib.path import Path
+from local_lib.path.path import Path
 
-if __name__ == '__main__':
-    new_dir = Path('test')
+if __name__ == "__main__":
+    new_dir = Path("test")
     new_dir.mkdir_p()
-    new_file = Path('test/test')
+    new_file = Path("test/test")
     new_file.touch()
-    new_file.write_bytes(b"Hello world!")
+    new_file.write_bytes(b"Hello world!\n")
     print(new_file.read_bytes().decode())
--- module03/ex01/my_script.sh
+++ module03/ex01/my_script.sh
@@ -3,15 +3,23 @@
 set -eu
 
 pip_version="$(pip --version)"
+lib_folder="local_lib"
 pathlib_url="https://github.com/jaraco/path.git"
-pathlib_folder_name="pathlib"
+pathlib_folder_name="path"
+
+function main {
+	if [ ! -d "$lib_folder/$pathlib_folder_name" ]; then
+		mkdir -p $lib_folder
+		echo "Cloning $pathlib_folder_name..."
+		git clone -v --depth 1 "$pathlib_url" "$lib_folder/$pathlib_folder_name" &>"$lib_folder/path.log"
+		echo "$pathlib_folder_name is cloned!"
+		python3 my_program.py
+	else
+		echo "$pathlib_folder_name is already here, removing it!"
+		rm -rf "$lib_folder"
+		main
+	fi
+}
 
 echo "pip version is $pip_version"
-if [ ! -d "local_lib/$pathlib_folder_name" ]; then
-	mkdir -p local_lib
-	echo "Cloning $pathlib_folder_name..."
-	git clone --depth 1 "$pathlib_url" "local_lib/$pathlib_folder_name" &>"local_lib/pathlib.log"
-	echo "$pathlib_folder_name is cloned!"
-else
-	echo "$pathlib_folder_name is already here!"
-fi
+main
