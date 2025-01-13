7b0a1647c43376e71e03571431609fa0cff4dfe3
tsiguenz <tsiguenz@student.42.fr>
2025-01-13 22:13:33 +0100

    d03 ex00 improve

--- module03/ex00/geohashing.py
+++ module03/ex00/geohashing.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 
 import sys
-import antigravity
 
-if __name__ == "__main__":
+
+def usage():
+    print("Usage: python3 geohashing.py <latitude> <longitude> <date>")
+    print(
+        'Exemple: python3 geohashing.py "37.421542" "122.085589" "2005-05-26010458.68"'
+    )
+
+
+def main():
     if len(sys.argv) != 4:
-        print("Usage: python3 geohashing.py <latitude> <longitude> <date>")
-        print(
-            'Exemple: python3 geohashing.py "37.421542" "122.085589" "2005-05-26010458.68"'
-        )
+        usage()
         sys.exit(1)
+
     try:
         latitude = float(sys.argv[1])
         longitude = float(sys.argv[2])
         date = sys.argv[3].encode()
+
+        import antigravity
+
         antigravity.geohash(latitude, longitude, date)
     except Exception as e:
         print("Error:", e)
+        usage()
+
+
+if __name__ == "__main__":
+    main()
